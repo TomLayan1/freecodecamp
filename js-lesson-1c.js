ccd53082235f80273bca3dc16d1486d4073b9426
TomLayan1 <afolayantomisin1919@gmail.com>
2023-09-01 22:01:34 +0100

    Concatenation and Interpolation practice

---
+++ js-lesson-1c.js
@@ -0,0 +1,9 @@
+    // Javascript Basics
+
+    // concatenation and interpoltion
+
+    // 1. Concatenation
+    let concatenation = 'may name is:' + ' Oluwatomisin';
+
+    // 2. Interpolation
+    let interpoltion = `Total cost: $${5 + 3}`;
