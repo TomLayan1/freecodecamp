4dd38f44beb99e8a62a215d565dbbed1bae68418
TomLayan1 <afolayantomisin1919@gmail.com>
2023-09-06 19:10:55 +0100

    Use of JavaScript If Statement

---
+++ js-lesson-1d.js
@@ -0,0 +1,11 @@
+let number =  10;
+const name = 'Tomisin';
+if (number >= 6 && number <= 12) {
+  console.log(`Good morning ${name}`);
+}
+else if (number >= 13 && number <= 17) {
+  console.log(`Good afternoon ${name}`);
+}
+else{
+  console.log(`Good night ${name}`);
+}
