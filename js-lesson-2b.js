f1a4a0fef09581d7f9d242bce071d8de9c2e6975
TomLayan1 <afolayantomisin1919@gmail.com>
2023-09-08 11:27:12 +0100

    localStorage added to the calculator

--- js-lesson-2b.js
+++ js-lesson-2b.js
@@ -1,16 +1,22 @@
 // a global variable that can be accessed outside scopes
-let total = '';
+let total = localStorage.getItem('total') || '';
 
 // for selecting numbers
 function getNumber(number){
   total += number;
   console.log(total);
+
+  localStorage.setItem('total', total);
 }
 
 // for making symbols perform different functions
 function pickSymbol(symbol){
   if (symbol === '='){
     total = eval(total);
+    console.log(total);
+  }
+  else if (symbol === '<x'){
+    total = total.slice(0, -1);
     console.log(total);
   }
 }
