21a313d2ac77dd8145c3f0a45117875f0cbea9bd
TomLayan1 <afolayantomisin1919@gmail.com>
2023-10-12 00:33:36 +0100

    first question to display on load and other question displays when each button is clicked

--- Project 2/script/quiz.js
+++ Project 2/script/quiz.js
@@ -2,85 +2,41 @@ import {question} from '../data/question.js';
 
 let quizHTML = '';
 
-question.forEach((question) =>{
-    quizHTML += `
+let firstQuestion = question[0];
+    quizHTML = `
     <div class="quiz-questionncontainer">
-      <p class="quiz-questionn js-quiz-question">${question.question}</p>
+      <p class="quiz-questionn js-quiz-question">${firstQuestion.question}</p>
       <img src="" alt="">
     </div>
     <div class="quiz-img-container">
       <img class="quiz-img" src="">
     </div>
     <div class="option-container">
-      <input class="option-selector" type="radio" name="quiz-option-${question.id}">
+      <input class="option-selector" type="radio" name="quiz-option-${firstQuestion.id}" value="A">
       <img src="" alt="" >
-      <p class="quiz-option-value">${question.option.a}</p>
+      <p class="quiz-option-value">${firstQuestion.option.a}</p>
     </div>
     <div class="option-container">
-      <input class="option-selector" type="radio" name="quiz-option}">
+      <input class="option-selector" type="radio" name="quiz-option-${firstQuestion.id}" value="B">
       <img src="" alt="" >
-      <p class="quiz-option-value">${question.option.b}</p>
+      <p class="quiz-option-value">${firstQuestion.option.b}</p>
     </div>
     <div class="option-container">
-      <input class="option-selector" type="radio" name="quiz-option">
+      <input class="option-selector" type="radio" name="quiz-option-${firstQuestion.id}" value="C">
       <img src="" alt="" >
-      <p class="quiz-option-value">${question.option.c}</p>
+      <p class="quiz-option-value">${firstQuestion.option.c}</p>
     </div>
     <div class="option-container">
-      <input class="option-selector" type="radio" name="quiz-option">
+      <input class="option-selector" type="radio" name="quiz-option-${firstQuestion.id}" value="D">
       <img src="" alt="" >
-      <p class="quiz-option-value">${question.option.d}</p>
-    </div>`;
-  });
-  // console.log(quizHTML);
-  
-  let buttonHTML = '';
-  question.forEach((question) =>{
-    buttonHTML += `
-    <button class="quiz-btn js-quiz-btn" data-quiz-id="${question.id}">1</button>
-    `;
-  });
-  // console.log(buttonHTML);
-
-  document.querySelector('.js-quiz-btn-container').innerHTML = buttonHTML;
-
-  document.querySelectorAll('.js-quiz-btn').forEach((button) =>{
-    button.addEventListener('click', () =>{
-      const questionId = button.dataset.quizId;
-      console.log(questionId);
-      document.querySelector('.js-quiz-display-container').innerHTML= questionId;
-    });
-  });
-
-
-
-/* <div class="quiz-questionncontainer">
-<p class="quiz-questionn js-quiz-question">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus doloribus nisi commodi repellat.</p>
-<img src="" alt="">
-</div>
-<div class="quiz-img-container">
-<img class="quiz-img" src="">
-</div>
-<div class="option-container">
-<input class="option-selector" type="radio" name="quiz-option">
-<img src="" alt="" >
-<p class="quiz-option-value">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-</div>
-<div class="option-container">
-<input class="option-selector" type="radio" name="quiz-option">
-<img src="" alt="" >
-<p class="quiz-option-value">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-</div>
-<div class="option-container">
-<input class="option-selector" type="radio" name="quiz-option">
-<img src="" alt="" >
-<p class="quiz-option-value">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-</div>
-<div class="option-container">
-<input class="option-selector" type="radio" name="quiz-option">
-<img src="" alt="" >
-<p class="quiz-option-value">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-</div>
-<div class="quiz-btn-container js-quiz-btn-container">
-<button class="quiz-btn js-quiz-btn">1</button>
-</div> */
+      <p class="quiz-option-value">${firstQuestion.option.d}</p>
+    </div>
+     `;
+    document.querySelector('.js-quiz-display-container').innerHTML = quizHTML;
+    document.querySelectorAll('.option-selector').forEach((radio) => {
+      radio.addEventListener('change', ()=>{
+        console.log('correct');
+        let questionButton = document.querySelectorAll('.quiz-btn');
+        questionButton.style.backgroundColor = 'black';  
+      })
+    })
