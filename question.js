fbfc8b5ca3b7794cbb6df1c5ade5bd33415ca222
TomLayan1 <afolayantomisin1919@gmail.com>
2023-10-27 11:30:35 +0100

    getting input from a field

---
+++ question.js
@@ -0,0 +1,113 @@
+export const question= [
+  {
+    id: "Q1",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q2",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q3",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q4",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q5",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q6",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q7",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q8",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q9",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+  {
+    id: "Q10",
+    question: "",
+    option:{
+      a: "",
+      b: "",
+      c: "",
+      d: ""
+    },
+    bestOption: ""
+  },
+]
+
