1ee6841f07e9c71570381a328b7db649c1409f0b
TomLayan1 <afolayantomisin1919@gmail.com>
2023-10-31 18:56:01 +0100

    contact page designed

--- Project 2/script/login.js
+++ Project 2/script/login.js
@@ -1,20 +1,15 @@
-// import {registeredUsers} from './signup.js';
+// get data from storage directly from the storege without exporting and importing
 let registeredUsers = JSON.parse(localStorage.getItem('registeredUsers'));
 if (!registeredUsers){
   registeredUsers = [];
 }
-console.log(registeredUsers[0]);
-
 
 // LOGIN VALIDATION
 let loginErrorMessage = document.querySelector('.js-login-error-message');
 
 let loginUsername = document.querySelector('.js-Username-field');
-console.log(loginUsername);
-
 
 let loginPassword = document.querySelector('.js-password-field');
-console.log(loginPassword);
 
 const loginButton = document.querySelector('.js-login-form-button');
 
@@ -31,9 +26,7 @@ loginButton.addEventListener('click', (e) => {
   })
   if(!successLogin){
     loginErrorMessage.innerHTML =  'Invalid Username or Password';
-      console.log('Not a success');
     }else{
       window.location.href = '/quiz.html'
-      console.log('success');
   }
 });
