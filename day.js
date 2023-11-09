50a6a01cd7e608a307033d7e758a20c8df639a71
TomLayan1 <afolayantomisin1919@gmail.com>
2023-11-09 15:08:35 +0100

    Using ECM version of dayjs in JS module to diplay the delivery date in the console

--- day.js
+++ day.js
@@ -1,4 +1,4 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 const today = dayjs();
-
 const deliveryDate = today.add(7, 'days');
 console.log(deliveryDate.format('dddd, MMMM D'));
