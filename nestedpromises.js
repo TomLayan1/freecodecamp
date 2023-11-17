e7939625ec9d65539cc18677e8c9525338bf60f6
TomLayan1 <afolayantomisin1919@gmail.com>
2023-11-17 02:49:45 +0100

    Getting the message after each promise is resolved

--- nestedpromises.js
+++ nestedpromises.js
@@ -3,21 +3,21 @@ let fuelTank = function(){
     resolve(`The car doesn't have enough fuel.`);
   })
 }
-let engine = function(){
+let engine = function(message){
   return new Promise (function(resolve, reject){
-    resolve(`The engine is over heating.`);
+    resolve(message + `The engine is over heating.`);
   })
 }
-let travel = function(){
+let travel = function(message){
   return new Promise (function(resolve, reject){
-    resolve(`The car is not fit for traveling.`);
+    resolve(message + `The car is not fit for traveling.`);
   })
 }
 
-fuelTank().then(function(){
-  return engine();
-}).then(function(){
-  return travel();
-}).then(function(){
-  document.write(`Done.`);
+fuelTank().then(function(result){
+  return engine(result);
+}).then(function(result){
+  return travel(result);
+}).then(function(result){
+  document.write(`Done.` + result);
 })
