41e378ff59ebee51001c9dd73e34f0aaf61b9ce6
TomLayan1 <afolayantomisin1919@gmail.com>
2023-11-18 09:05:06 +0100

    Comparing two values to check if they are of the same type

---
+++ JavaScipt/Comparing-values.js
@@ -0,0 +1,10 @@
+const a = 20;
+const b = 30;
+
+function comparevalues(a, b){
+  if(a === b)
+    return 'Values are the same';
+  else
+    return 'Values are different';
+}
+console.log(comparevalues(a, b));
---
+++ JavaScipt/Conditionals.js
@@ -0,0 +1,6 @@
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue)
+    return 'Yes, that was true';
+  else
+    return 'No, that was false';
+}
