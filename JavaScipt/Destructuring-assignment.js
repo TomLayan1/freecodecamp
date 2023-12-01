ccaac535f2c407b200627ce43e909f79553f6e33
TomLayan1 <afolayantomisin1919@gmail.com>
2023-12-01 23:37:46 +0100

    Use Destructuring Assignment to Extract Values from Objects

---
+++ JavaScipt/Destructuring-assignment.js
@@ -0,0 +1,8 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+const {yesterday, today, tomorrow, } = HIGH_TEMPERATURES;
+console.log(yesterday, today, tomorrow);
