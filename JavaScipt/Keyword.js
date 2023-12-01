ff0ba6123377fbad3a6f185f032321b0f46edda7
TomLayan1 <afolayantomisin1919@gmail.com>
2023-12-01 02:27:17 +0100

    Mutate an Array Declared with const

--- JavaScipt/Keyword.js
+++ JavaScipt/Keyword.js
@@ -1,7 +1,7 @@
 function checkScope() {
   let i = "function scope";
   if (true) {
-    let i = 'block scope';
+   i = 'block scope';
     console.log('Block scope i is: ', i);
   }
   console.log('Function scope i is: ', i);
