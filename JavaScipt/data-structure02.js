0952b2efd7beedb7ef0f58c2b186bfef7def4c86
TomLayan1 <afolayantomisin1919@gmail.com>
2024-01-05 15:48:05 +0100

    Removing items using splice()

---
+++ JavaScipt/data-structure02.js
@@ -0,0 +1,5 @@
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// .splice() takes two inputs 1: the index to start from 2: the of items to remove starting from that index
+arr.splice(1, 4);
+
+console.log(arr);
