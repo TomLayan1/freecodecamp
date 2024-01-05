f718306db1a0be9a67e5350950534a5707997735
TomLayan1 <afolayantomisin1919@gmail.com>
2024-01-05 17:15:18 +0100

    Iterating through all an array items using for loop

---
+++ JavaScipt/data-structure08.js
@@ -0,0 +1,14 @@
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+  //Checks every array item for the element and if is not there continues the code
+    if (arr[i].indexOf(elem) === -1) {
+    //Inserts the element of the array in newArr
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+Iterate through all an array's items using for loops
