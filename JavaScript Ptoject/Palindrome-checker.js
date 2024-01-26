a8f72600bf82acd1ff63176e284b30281e768419
TomLayan1 <afolayantomisin1919@gmail.com>
2024-01-26 11:58:09 +0100

    A cash register

--- JavaScript Ptoject/Palindrome-checker.js
+++ JavaScript Ptoject/Palindrome-checker.js
@@ -1,8 +1,21 @@
+const input = document.getElementById('text-input');
+const output = document.getElementById('result');
+const button = document.getElementById('check-btn');
+
+button.addEventListener('click', () => {
+  const str = input.value;
+  palindrome(str)
+})
+
 function palindrome(str) {
   const fixedString = str.replace(/\W+|_/g, '').toLowerCase();
   const result = fixedString.split('').reverse().join('');
   console.log(fixedString, result);
-  return (fixedString === result) ? true : false
+  if (fixedString === result) {
+   output.innerHTML = str + ' is a palindrome';
+  } else {
+   output.innerHTML = str + ' is not a palindrome';
+  }
 }
 
-console.log(palindrome("result"));
+// console.log(palindrome("result"));
