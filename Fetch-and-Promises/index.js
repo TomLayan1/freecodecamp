52caa7488b5d0a8f7a79122ad137e4b0713b7b09
TomLayan1 <afolayantomisin1919@gmail.com>
2024-02-08 00:13:24 +0100

    using the json method on the response object to parse it into JSON

--- Fetch-and-Promises/index.js
+++ Fetch-and-Promises/index.js
@@ -1,5 +1,10 @@
+// select the author container and load more button with the dom
 const authorContainer = document.getElementById('author-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
 
+// use the fetch method and pass a server url in it to get data from the server
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
-  .then((res)=> console.log(res))
+
+  // use the .then() method to get a response
+  // to make the data usable, you can use the .json() method on the response object to parse it into JSON
+  .then((res) => res.json())
