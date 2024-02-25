49e7ea54f92e144915bb3ea2dffa1e43015dc19c
TomLayan1 <afolayantomisin1919@gmail.com>
2024-02-25 10:10:41 +0100

    Checked if author bio is longer than 50. If author bio longer than 50, the remaing character will be replaced by ellipses

--- Fetch-and-Promises/index.js
+++ Fetch-and-Promises/index.js
@@ -57,7 +57,8 @@ const displayAuthors = (authors) => {
       <div id="${index}" class="user-card">
         <h2 class="author-name">${author}</h2>
         <img class="user-img" alt="${author} avatar" src="${image}">
-        <p class="bio">${bio}</p>
+        <div class="purple-divider"></div>
+        <p class="bio">${bio.length > 50? bio.slice(0, 50) + '...' : bio}</p>
         <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
       </div>
     `;
