d85f020e78977deb96444fd95b1ecd0910dd71ca
TomLayan1 <afolayantomisin1919@gmail.com>
2024-02-27 04:18:17 +0100

    HTML for fcc forum loderboard

---
+++ Asynchronous-Programming/index.js
@@ -0,0 +1,10 @@
+// Declare four const variable
+const forumLatest = 'https://forum-proxy.freecodecamp.rocks/latest';
+const forumTopicUrl = 'https://forum.freecodecamp.org/t/';
+const forumCategoryUrl = 'https://forum.freecodecamp.org/c/';
+const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';
+
+const postsContainer = document.getElementById('posts-container');
+
+// To populate the forum leaderboard with data, you will need to request the data from an API
+const fetchData = async () => {};
