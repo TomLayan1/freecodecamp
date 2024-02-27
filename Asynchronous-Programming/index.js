9d3ba0ced71224eb5f2d5eaa7fd92b92c2d4532d
TomLayan1 <afolayantomisin1919@gmail.com>
2024-02-27 14:28:14 +0100

    Used the await keyword to handle the asynchronous nature of the fetch method

--- Asynchronous-Programming/index.js
+++ Asynchronous-Programming/index.js
@@ -7,4 +7,9 @@ const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';
 const postsContainer = document.getElementById('posts-container');
 
 // To populate the forum leaderboard with data, you will need to request the data from an API
-const fetchData = async () => {};
+const fetchData = async () => { 
+  try{
+    // Use the await keyword to handle the asynchronous nature of the fetch() method
+    const res = await fetch(forumLatest);
+  } catch (err) {}
+};
