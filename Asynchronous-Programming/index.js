f22dad4e8933b01c1c31fdc46334c23dca2b952d
TomLayan1 <afolayantomisin1919@gmail.com>
2024-03-02 09:17:08 +0100

    Used destructuring to get topic list and users from data

--- Asynchronous-Programming/index.js
+++ Asynchronous-Programming/index.js
@@ -12,6 +12,9 @@ const fetchData = async () => {
     // Use the await keyword to handle the asynchronous nature of the fetch() method
     const res = await fetch(forumLatest);
     const data = await res.json()
+
+    // Call the showLatestPosts() function in the try block to see your changes.
+    showLatestPosts(data);
   } catch (err) {}
 };
 // call the fetchData function to log the data
@@ -19,5 +22,6 @@ fetchData();
 
 // To dislay the data, we start by creating an arrow function which takes in a data parameter
 const showLatestPosts = (data) => {
-
+  // use destructuring to get the topic_list and users properties from the data object.
+  const { topic_list, users } = data;
 }
