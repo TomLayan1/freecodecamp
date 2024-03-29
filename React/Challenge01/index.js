665189efc988de8c1759691dbdfcd9de09eaa9b8
TomLayan1 <afolayantomisin1919@gmail.com>
2024-03-29 11:39:18 +0100

    Challenge 01

---
+++ React/Challenge01/index.js
@@ -0,0 +1,12 @@
+const navbar = (
+  <nav>
+    <h1>ShopIt</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById('main'));
