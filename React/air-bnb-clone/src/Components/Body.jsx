0992969cc4420d919d7cdebb2c929e459d3f6db9
TomLayan1 <afolayantomisin1919@gmail.com>
2024-04-05 11:26:06 +0100

    Homepage banner completed

---
+++ React/air-bnb-clone/src/Components/Body.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <section className='home'>
+        <h1 className='home-title'>Online Experiences</h1>
+        <p className='sub-title'>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
+      </section>
+    );
+  }
+}
+ 
+export default Body;
--- React/air-bnb-clone/src/index.js
+++ React/air-bnb-clone/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './Components/Header';
+import Body from './Components/Body';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
+    <Body />
   </React.StrictMode>
 );
