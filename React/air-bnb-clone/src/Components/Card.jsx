b86c389bff01fa3ad519ddfb463ab0c4f5208bdf
TomLayan1 <afolayantomisin1919@gmail.com>
2024-04-06 14:13:31 +0100

    Designed the card section to have a grid layout

--- React/air-bnb-clone/src/Components/Card.jsx
+++ React/air-bnb-clone/src/Components/Card.jsx
@@ -20,6 +20,36 @@ function CreateCard() {
           <p className='price'><span className='price-start'>From $136</span> / person</p>
         </div>
       </div>
+
+      <div className='card-bx-main'>
+        <div className='img-bx'>
+          <img className='item-img' src={itemImg}/>
+          <p className='sold-out'>SOLD OUT</p>
+        </div>
+        <div className='others-bx'>
+          <div className='star-bx'>
+            <img className='star' src={star}/>
+            <p className='rate-country'>5.0 <span className='location'>(6).USA</span></p>
+          </div>
+          <p className='item-name'>Life lessons with Katie Zaferes</p>
+          <p className='price'><span className='price-start'>From $136</span> / person</p>
+        </div>
+      </div>
+
+      <div className='card-bx-main'>
+        <div className='img-bx'>
+          <img className='item-img' src={itemImg}/>
+          <p className='sold-out'>SOLD OUT</p>
+        </div>
+        <div className='others-bx'>
+          <div className='star-bx'>
+            <img className='star' src={star}/>
+            <p className='rate-country'>5.0 <span className='location'>(6).USA</span></p>
+          </div>
+          <p className='item-name'>Life lessons with Katie Zaferes</p>
+          <p className='price'><span className='price-start'>From $136</span> / person</p>
+        </div>
+      </div>
     </section>
   );
 }
