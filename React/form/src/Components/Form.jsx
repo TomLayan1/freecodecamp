c7871f9335b5fd47638915a8ddeffb146ec54116
TomLayan1 <afolayantomisin1919@gmail.com>
2024-04-18 17:55:49 +0100

    Destructured the field attributes and used the ternary operator determine if a field will use a checked or value attribute

--- React/form/src/Components/Form.jsx
+++ React/form/src/Components/Form.jsx
@@ -7,26 +7,34 @@ const Form = () => {
     firstName: '',
     lastName: '',
     email: '',
-    comment: ''
+    comment: '',
+    isFriendly: true
   })
   console.log(formData);
 
   const handleChange = (event) => {
+    // destructure structure the field attributes and set them = event.target
+    const {name, value, type, checked} = event.target
     // pass a parameter to refers to the previous value of the state object
     // so you can get access to the current or previous value of the object
     setFormData((prevFormData) => {
       // return an object that returns the previous value of the state object and the changes made to specific properties
-      return {...prevFormData, [event.target.name]: event.target.value}
+      // use the ternary operator to choose if the field will use a checked or value to get the changes made
+      return {...prevFormData, [name]: type === 'checkbox' ? checked : value}
     })
   }
 
   return (
     <div>
       <form action='' method='' id=''>
+        {/* add value to the inputs and textarea elements to make them controlled components */}
         <input type='text' name='firstName' placeholder='First Name' value={formData.firstName} onChange={handleChange}/>
         <input type='text' name='lastName' placeholder='Last Name' value={formData.lastName} onChange={handleChange}/>
         <input type='email' name='email' placeholder='Enter Email Here' value={formData.email} onChange={handleChange}/>
         <textarea name='comment' placeholder='Comment' value={formData.comment} onChange={handleChange}/>
+        <label htmlFor='isFriendly'>
+        <input type='checkbox' id='isFriendly' name='isFriendly' checked={formData.isFriendly} onChange={handleChange}/>
+        Are you friendly?</label>
       </form>
     </div>
   )
