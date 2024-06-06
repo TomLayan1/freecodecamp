f85dbb10858e7c3f6259cc009952b12455eb28d2
TomLayan1 <afolayantomisin1919@gmail.com>
2024-06-06 16:34:59 +0100

    I hosted my json file using a local port

--- API/api/src/App.js
+++ API/api/src/App.js
@@ -2,13 +2,30 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [blog, setBlog] = useState(null)
+  const [blog, setBlog] = useState(null);
+  console.log(blog)
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data)
+        setBlog(data);
+      })
+  }, [])
+
   return (
     <>
       <div className='main'>
-        Blog Posts
+        BLOG POSTS
+        {blog.map(blog => (
+          <div key={blog.id}>
+            <p>{blog.title}</p>
+            <button>Delete</button>
+          </div>
+        ))}
       </div>
     </>
   )
